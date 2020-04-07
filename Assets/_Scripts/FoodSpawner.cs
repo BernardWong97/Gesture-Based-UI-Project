08866ba95c74482e30862a3121c3d2c73e7e2a4a
BernardWong97 <ben.wc88@gmail.com>
2020-04-07 17:52:20 +0100

    food spawner

---
+++ Assets/_Scripts/FoodSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    private float maxTime = 2;
+    private float minTime = 0.2f;
+    private float time;
+    private float spawnTime;
+    private float foodIndex;
+    public GameObject[] foods;
+
+    private GameObject foodNow;
+
+    void FixedUpdate()
+    {
+        time += Time.deltaTime;
+
+        if (time >= spawnTime)
+        {
+            foodNow = foods[Random.Range(0, foods.Length - 1)];
+            Debug.Log("Time to spawn: " + foodNow.name);
+            Spawn();
+            SetRandomTime();
+            time = 0;
+        }
+    }
+    
+    private void Start()
+    {
+        SetRandomTime();
+        time = 0;
+    }
+
+    private void SetRandomTime()
+    {
+        spawnTime = Random.Range(minTime, maxTime);
+        Debug.Log("Next food spawn in " + spawnTime + " seconds.");
+    }
+
+    
+
+    private void Spawn()
+    {
+        Instantiate(foodNow, transform.position, transform.rotation);
+    }
+}
