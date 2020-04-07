0b34ef1c552981d35ebd3aa4b97e7899e0663e77
BernardWong97 <ben.wc88@gmail.com>
2020-04-07 20:00:48 +0100

    Added score overlay

--- Assets/_Scripts/Food.cs
+++ Assets/_Scripts/Food.cs
@@ -1,11 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Food : MonoBehaviour
 {
+    private Text scoreUI;
+    private float score;
+
+    private void Start()
+    {
+        scoreUI = GameObject.Find("Points").GetComponent<Text>();
+    }
+
     private void OnMouseDown()
     {
+        score = Convert.ToInt32(scoreUI.text);
+        score += 10;
+        scoreUI.text = score.ToString();
         Destroy(this.gameObject);
     }
 }
