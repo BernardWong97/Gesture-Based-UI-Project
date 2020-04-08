125ec416e9e8164d18f87cf68e8def14dc3d1408
BernardWong97 <ben.wc88@gmail.com>
2020-04-08 16:10:49 +0100

    Save volume preference

--- Assets/_Scripts/Settings.cs
+++ Assets/_Scripts/Settings.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider musicSlider, gameSlider;
     public void SetMusic(float volume)
     {
         audioMixer.SetFloat("musicVolume", volume);
+        PlayerPrefs.SetFloat("music", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("gameVolume", volume);
+        PlayerPrefs.SetFloat("game", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("music");
+        gameSlider.value = PlayerPrefs.GetFloat("game");
     }
 }
