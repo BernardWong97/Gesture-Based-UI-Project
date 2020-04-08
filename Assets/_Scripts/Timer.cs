b9ff33df53a8c869dab148c4007ba583820ce916
BernardWong97 <ben.wc88@gmail.com>
2020-04-08 11:55:08 +0100

    Added countdown timer

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI tmp;
+    private float timer = 10;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    private void Start()
+    {
+        tmp = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if(timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            tmp.text = Math.Round(timer, 0, MidpointRounding.AwayFromZero).ToString();
+        }
+        else if (timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            tmp.text = "0";
+            timer = 0.0f;
+            Debug.Log("Times Up"); // Game Over
+        }
+    }
+}
